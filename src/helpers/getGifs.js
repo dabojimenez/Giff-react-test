f199a4acffe468ba2e8ccda9eeb21ab2bbed16d1
David Jimenez <dabojimenez16@gmail.com>
2023-06-11 17:06:47 -0500

    se agregan la siantaaciones necesarias para realizar las pruebas de la aplicacion

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,6 +1,6 @@
 export const getGifs = async( category ) => {
 
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=10`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=OxjjAn7Si1An7ZtkF2xdyoRDujEz7cr5&q=${category}&limit=10`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
